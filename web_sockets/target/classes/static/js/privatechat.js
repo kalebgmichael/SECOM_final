cc0b560a94045896e2929f902942d9986ba1d7bb
kalebgmichael <kalebgmac@gmail.com>
2024-09-30 16:53:56 +0200

    fixed websocket static content: splict html and js: exported a new one the realm-export.json

---
+++ web_sockets/target/classes/static/js/privatechat.js
@@ -0,0 +1,73 @@
+         var user1= "00002";
+         var user2= "0001";
+   stompClient.subscribe('/user1/specific/private-chat', function (message) {
+            console.log("Subscribed to: " + message.headers.destination);
+            console.log("User: " + message.headers.user); // Assuming 'user' is a header sent by the server
+            console.log("Message from topic: " + message);
+            showMessage_private('Message received: ' + message.body);
+            // Extract private key from the received message
+            const privateKey = JSON.parse(message.body).text;
+            });
+
+   stompClient.subscribe('/user2/specific/private-chat', function (message) {
+            console.log("Subscribed to: " + message.headers.destination);
+            console.log("User: " + message.headers.user); // Assuming 'user' is a header sent by the server
+            console.log("Message from topic: " + message);
+            showMessage_private('Message received: ' + message.body);
+            // Extract private key from the received message
+            const privateKey = JSON.parse(message.body).text;
+            });
+
+<!--     stompClient.subscribe('/topic/public-key', function (message) {-->
+<!--            console.log("Subscribed to: " + message.headers.destination);-->
+<!--            console.log("User: " + message.headers.user); // Assuming 'user' is a header sent by the server-->
+<!--            console.log("Message from topic: " + message);-->
+<!--            showMessage_private('Message received: ' + message.body);-->
+<!--            // Extract private key from the received message-->
+<!--            const privateKey = JSON.parse(message.body).text;-->
+<!--            });-->
+
+
+
+
+        // Send a message
+    var message = {
+        sender: 'kaleb',
+        rec: '00002',
+        text: 'MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAIBAtCMdVcFMVgV/OUgq+4EJfqZmYBB+8akBVIaGddn2DdOFrgUqyL0FNSXPbPA+2TMg0t9RghE9/vVDmEKThjtABwkfcC+JOcJqwDAbsoEPMZgUz+RrnaSKiX/S8xVKV7Wj131lPMX72kn4aSgXnKQUXrbLuavH3KikVK5x7NoJAgMBAAECgYA2uWUjxpSc0jGyTsLmZFDEkoSUBALhhwkekA69CAqpYjAsHVJPqh3Vaa9v3r4hFPAgvNS9rU3OhaGQjbMeVUxkx9GVt2LUnGMX/M5yFh7OZl7cMtebhoeEt0z5SXI3NULiEKKjHgXwTc4DaA+lcp/gxSNDQNlSFJWhfvTUQDqosQJBAJ8nPN0eOesTaCCzATO3FpkcuhQ9XLhAQTvbzYb67gtK+4Vh8pBAEFLEuKZAmBidub63Lv50cLnm/1L7+4Y6+H8CQQDOS871vu+n914il1GBZ81IqbwKki3IWx/d6iXz1UlXyOEMgniNpQYKW8EWzvrT/lXDlTe3VKgpBfllpgIBySl3AkBzmfmglxr0wDTrQ3qFCOEWOAKFPwkBIFMB2qdP+yY696z4dmvNEWuJ4zBIOjT/9Fj9yWsOEp/quHoO2c8Z8e2bAkB05i5bwRuq8ZjNPzP3gWupXk1pLBZ3b3OqW7Gv70/FR9aHMTPBCB9ZJU9Qbm9iS8AruVW+NGGqBXGisSR4AJbXAkAjPxbNZsSfeJZY/AkU2SNbIvHnLACQvRwJwibvzAkDfJ4t1gVohi+enBO91slbMT+tQGD9qnLxDJpso4B3rmSG'
+         };
+         console.log("this is message"+message);
+         console.log("this is message"+message);
+
+                 // Send a message
+    var message1 = {
+        sender: 'kaleb',
+        rec: '00001',
+        text: 'MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAIBAtCMdVcFMVgV/OUgq+4EJfqZmYBB+8akBVIaGddn2DdOFrgUqyL0FNSXPbPA+2TMg0t9RghE9/vVDmEKThjtABwkfcC+JOcJqwDAbsoEPMZgUz+RrnaSKiX/S8xVKV7Wj131lPMX72kn4aSgXnKQUXrbLuavH3KikVK5x7NoJAgMBAAECgYA2uWUjxpSc0jGyTsLmZFDEkoSUBALhhwkekA69CAqpYjAsHVJPqh3Vaa9v3r4hFPAgvNS9rU3OhaGQjbMeVUxkx9GVt2LUnGMX/M5yFh7OZl7cMtebhoeEt0z5SXI3NULiEKKjHgXwTc4DaA+lcp/gxSNDQNlSFJWhfvTUQDqosQJBAJ8nPN0eOesTaCCzATO3FpkcuhQ9XLhAQTvbzYb67gtK+4Vh8pBAEFLEuKZAmBidub63Lv50cLnm/1L7+4Y6+H8CQQDOS871vu+n914il1GBZ81IqbwKki3IWx/d6iXz1UlXyOEMgniNpQYKW8EWzvrT/lXDlTe3VKgpBfllpgIBySl3AkBzmfmglxr0wDTrQ3qFCOEWOAKFPwkBIFMB2qdP+yY696z4dmvNEWuJ4zBIOjT/9Fj9yWsOEp/quHoO2c8Z8e2bAkB05i5bwRuq8ZjNPzP3gWupXk1pLBZ3b3OqW7Gv70/FR9aHMTPBCB9ZJU9Qbm9iS8AruVW+NGGqBXGisSR4AJbXAkAjPxbNZsSfeJZY/AkU2SNbIvHnLACQvRwJwibvzAkDfJ4t1gVohi+enBO91slbMT+tQGD9qnLxDJpso4B3rmSG'
+         };
+         console.log("this is message"+message1);
+         console.log("this is message"+message1);
+
+    var message3 = {
+        senderId: 'kaleb',
+        publicKey: 'MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAIBAtCMdVcFMVgV/OUgq+4EJfqZmYBB+8akBVIaGddn2DdOFrgUqyL0FNSXPbPA+2TMg0t9RghE9/vVDmEKThjtABwkfcC+JOcJqwDAbsoEPMZgUz+RrnaSKiX/S8xVKV7Wj131lPMX72kn4aSgXnKQUXrbLuavH3KikVK5x7NoJAgMBAAECgYA2uWUjxpSc0jGyTsLmZFDEkoSUBALhhwkekA69CAqpYjAsHVJPqh3Vaa9v3r4hFPAgvNS9rU3OhaGQjbMeVUxkx9GVt2LUnGMX/M5yFh7OZl7cMtebhoeEt0z5SXI3NULiEKKjHgXwTc4DaA+lcp/gxSNDQNlSFJWhfvTUQDqosQJBAJ8nPN0eOesTaCCzATO3FpkcuhQ9XLhAQTvbzYb67gtK+4Vh8pBAEFLEuKZAmBidub63Lv50cLnm/1L7+4Y6+H8CQQDOS871vu+n914il1GBZ81IqbwKki3IWx/d6iXz1UlXyOEMgniNpQYKW8EWzvrT/lXDlTe3VKgpBfllpgIBySl3AkBzmfmglxr0wDTrQ3qFCOEWOAKFPwkBIFMB2qdP+yY696z4dmvNEWuJ4zBIOjT/9Fj9yWsOEp/quHoO2c8Z8e2bAkB05i5bwRuq8ZjNPzP3gWupXk1pLBZ3b3OqW7Gv70/FR9aHMTPBCB9ZJU9Qbm9iS8AruVW+NGGqBXGisSR4AJbXAkAjPxbNZsSfeJZY/AkU2SNbIvHnLACQvRwJwibvzAkDfJ4t1gVohi+enBO91slbMT+tQGD9qnLxDJpso4B3rmSG',
+        recId: '00001'
+         };
+
+    stompClient.send('/chat-app/api/socket/chat', {}, JSON.stringify(message));
+        // Get the username from a hidden input field injected by the server-side code
+        const ownerId = document.getElementById('ownerId').innerText;
+        // Now you can use the username in your JavaScript code
+        console.log("Logged in ownerId:", ownerId);
+
+<!--    stompClient.send('/chat-app/api/socket/private-chat', {}, JSON.stringify(message1));-->
+<!--        // Get the username from a hidden input field injected by the server-side code-->
+<!--        const ownerId1 = document.getElementById('ownerId').innerText;-->
+<!--        // Now you can use the username in your JavaScript code-->
+<!--        console.log("Logged in ownerId_private:", ownerId1);-->
+
+<!--    stompClient.send('/chat-app/api/socket/public-key', {}, JSON.stringify(message3));-->
+<!--        // Get the username from a hidden input field injected by the server-side code-->
+<!--        const ownerId2 = document.getElementById('ownerId').innerText;-->
+<!--        // Now you can use the username in your JavaScript code-->
+<!--        console.log("Logged in ownerId:", ownerId1);-->
